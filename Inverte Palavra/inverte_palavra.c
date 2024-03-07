67700dd7c7c74731b1584886574ed58de307a2e3
uilham-max <uilhamgoncalves@gmail.com>
2024-03-07 17:14:16 -0300

    Primeiro envio

--- Inverte Palavra/inverte_palavra.c
+++ Inverte Palavra/inverte_palavra.c
@@ -2,12 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 
-// Função para inverter uma palavra
 void inverterPalavra(char palavra[]) {
     int comprimento = strlen(palavra);
     int i, j;
 
-    // Trocar os caracteres da palavra da posição inicial até a metade
     for (i = 0, j = comprimento - 1; i < j; i++, j--) {
         char temp = palavra[i];
         palavra[i] = palavra[j];
@@ -18,14 +16,11 @@ void inverterPalavra(char palavra[]) {
 int main() {
     char palavra[100];
 
-    // Solicitar a entrada da palavra
     printf("Digite uma palavra: ");
     scanf("%s", palavra);
 
-    // Inverter a palavra
     inverterPalavra(palavra);
 
-    // Exibir a palavra invertida
     printf("Palavra Invertida: %s\n", palavra);
 
     return 0;
